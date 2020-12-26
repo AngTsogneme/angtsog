cb5d32f98cb5adbcc0322def5483ffb084a8b34a
Angarag <angarag97@gmail.com>
2020-12-26 01:34:41 -0500

    update

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -39,7 +39,7 @@ class MainPage extends Component{
   
                 
                 <h1 className = "h1_main_page" >
-                    <img className = "profile_pic" src = {require("../images/profile.JPEG")} alt = "profilefck"/>
+                    <img className = "profile_pic" src = {require("../images/profile.jpeg")} alt = "profile"/>
                     Hi! I'm <p className = "name">Ang Tsog</p>
 
                 </h1>
